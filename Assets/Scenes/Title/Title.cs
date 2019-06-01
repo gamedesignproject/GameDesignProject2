c3b411a99f3fb7e98db22b01a3b4df5a5d12bf7c
YusukeHirashima <50046407+YusukeHirashima@users.noreply.github.com>
2019-06-01 19:39:00 +0900

    Revert "TitleUI"
    This reverts commit db4e6e567d16f1f5a72745135896871df6f89b70.

--- Assets/Scenes/Title/Title.cs
+++ Assets/Scenes/Title/Title.cs
@@ -1,27 +1,48 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 
 public class Manager : MonoBehaviour
 {
+    // Playerプレハブ
+    public GameObject player;
+
     // タイトル
-    private GameObject Title;
-
-
+    private GameObject title;
 
     void Start()
+    {
+        // Titleゲームオブジェクトを検索し取得する
+        title = GameObject.Find("Title");
+    }
+
+    void OnGUI()
+    {
+        // ゲーム中ではなく、タッチまたはマウスクリック直後であればtrueを返す。
+        if (IsPlaying() == false && Event.current.type == EventType.MouseDown)
         {
-
-        SceneManager.LoadScene("Title");
-
+            GameStart();
         }
+    }
 
-       
-        void Update()
-        {
+    void GameStart()
+    {
+        // ゲームスタート時に、タイトルを非表示にしてプレイヤーを作成する
+        title.SetActive(false);
+        Instantiate(player, player.transform.position, player.transform.rotation);
+    }
 
-        }
+    public void GameOver()
+    {
+        //FindObjectOfType<Score>().Save();
+        //// ゲームオーバー時に、タイトルを表示する
+        //title.SetActive(true);
+    }
 
-
+    public bool IsPlaying()
+    {
+        // ゲーム中かどうかはタイトルの表示/非表示で判断する
+        return title.activeSelf == false;
+    }
 }
 
